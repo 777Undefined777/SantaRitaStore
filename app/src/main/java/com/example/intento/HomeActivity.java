c4a1d9d8f06371c9e8fddc74ab59d7d888d258f3
jorge <jorgeandresmenesespenaranda@gmail.com>
2024-05-21 18:25:35 -0500

    commit init

--- app/src/main/java/com/example/intento/HomeActivity.java
+++ app/src/main/java/com/example/intento/HomeActivity.java
@@ -38,7 +38,7 @@ public class HomeActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(view -> {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Boton En Construccion", Snackbar.LENGTH_LONG)
                     .setAction("Action", null)
                     .setAnchorView(R.id.fab).show();
         });
